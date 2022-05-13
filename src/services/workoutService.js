0765e6445e371ba30f2f4502b8dc1a57b6e1fd78
José William <jwmeneses@sfiec.org.br>
2022-05-13 17:13:21 -0300

    Get all workouts and create new workout

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -1,13 +1,25 @@
+const { v4: uuid } = require("uuid")
+const Workout = require("../database/Workout")
+
 const getAllWorkouts = () => {
-  return
+  return Workout.getAllWorkouts()
 }
 
 const getOneWorkout = () => {
   return
 }
 
-const createNewWorkout = () => {
-  return
+const createNewWorkout = (newWorkout) => {
+  const workoutToInsert = {
+    ...newWorkout,
+    id: uuid(),
+    createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+    updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+  }
+
+  const createdWorkout = Workout.createNewWorkout(workoutToInsert)
+
+  return createdWorkout
 }
 
 const updateOneWorkout = () => {
